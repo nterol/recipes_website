890696eebcf84089239a08ae8109c69fe4a91b63
nterol <n.terol@24sevres.com>
2020-05-13 19:05:41 +0200

    🔧 more config

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -2,4 +2,6 @@ import CMS from 'netlify-cms-app';
 
 import RecipePagePreview from './preview-template/RecipePagePreview';
 
+CMS.registerPreviewStyle('../layout/css/layout.css');
+
 CMS.registerPreviewTemplate('recipes', RecipePagePreview)
